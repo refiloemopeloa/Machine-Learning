27695459936c74323633f00b88a84da96f6e76a7
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-03-28 14:27:23 +0200

    Added method for training perceptron - Created method for generating weights and threshold. - Created method for sample input. - Created method for simulating one node. - Created method for calculating loss. - Created method for training perceptron. - Created method for printing state and data.

--- Neural-Networks/Perceptron-Training/Perceptron-Training.py
+++ Neural-Networks/Perceptron-Training/Perceptron-Training.py
@@ -21,6 +21,64 @@ def generate_targets(N, dataset):
         else:
             T[row] = 1
 
+    return T
+
+def sample_input():
+    X = np.array([[0,0],[0,1],[1,0],[1,1]])
+    T = np.array([1,1,1,0])
+
+    return X,T
+
+def generate_weights(m):
+    weights = np.empty(shape=(m,1),dtype=float)
+    for row in range(m):
+        weight = np.random.uniform(-1,1)
+        weights[row] = weight
+
+    return weights
+
+def generate_threshold():
+    return np.random.uniform(-1,1)
+
+def perceptron(x, weights, threshold):
+    sum = 0
+    for i in range(len(x)):
+        sum += x[i] * weights[i]
+    if sum > threshold:
+        return 1
+    else:
+        return 0
+
+def loss(t, y):
+    return t - y
+
+def train_perceptron(m, N, X, weights, targets, threshold, learning_rate, stop_condition):
+    L1 =0
+    L2 = 0
+    epochs = 0
+    while True:
+        for i in range(N):
+            y = perceptron(X[i,:], weights, threshold)
+            for j in range(m):
+                weights[j] += learning_rate * (targets[i] - y) * X[i,j]
+            threshold -= learning_rate * (targets[i] - y)
+            L2 = L1
+            L1 += loss(targets[i], y)
+        epochs += 1
+        if np.abs(L2-L1) <= stop_condition:
+            break
+
+    return weights, threshold, epochs
+
+def print_state(weights, threshold):
+    print("Weights:\n",weights)
+    print("Threshold:\n",threshold)
+    print("=====================")
+
+def print_data(X, T):
+    print("X\tT")
+    for i in range(len(T)):
+        print(X[i], "\t", T[i])
 
 def main():
     N = int(input("N:\t"))
